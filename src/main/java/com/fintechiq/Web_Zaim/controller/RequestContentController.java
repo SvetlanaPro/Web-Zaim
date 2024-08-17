d5e98f20ab27d06c66b1bae0f7ffcefe656fdcc9
S.Riabukhina <svetlana.prokoptsova@gmail.com>
2024-08-17 10:21:28 +1000

    added controller

--- src/main/java/com/fintechiq/Web_Zaim/controller/RequestContentController.java
+++ src/main/java/com/fintechiq/Web_Zaim/controller/RequestContentController.java
@@ -1,6 +1,7 @@
 package com.fintechiq.Web_Zaim.controller;
 
 import com.fintechiq.Web_Zaim.service.RequestContentService;
+import com.fintechiq.Web_Zaim.service.StopFactorCalculator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import java.io.IOException;
 public class RequestContentController {
 
     private final RequestContentService requestContentService;
+    private final StopFactorCalculator stopFactorCalculator;
 
     @PostMapping
     public void saveRequestContent(@RequestBody String jsonContent) throws IOException {
@@ -27,5 +29,9 @@ public class RequestContentController {
     public String getRequestContent(@PathVariable Long id) {
         var requestContent = requestContentService.getRequestContent(id);
         return requestContent != null ? requestContent.getContent() : null;
+    }
+    @GetMapping("/calculator")
+    public boolean calculatorStopFactor( String regPerson, String verifiedName){
+        return stopFactorCalculator.calculatorStopFactor(regPerson, verifiedName);
     }
 }
